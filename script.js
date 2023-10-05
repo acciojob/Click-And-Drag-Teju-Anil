d9630ddaa41d77a6152121ff6d34f10e61a31a74
Teju-Anil <127083204+Teju-Anil@users.noreply.github.com>
2023-10-05 22:03:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Your code here.
+ function handleMouseUp() {
+            isDragging = false;
+        }
+
+        document.querySelectorAll('.cube').forEach(cube => {
+            cube.addEventListener('mousedown', handleMouseDown);
+            document.addEventListener('mousemove', handleMouseMove);
+            document.addEventListener('mouseup', handleMouseUp);
+        });
